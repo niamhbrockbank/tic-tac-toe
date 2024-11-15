c2860df1f3c3f91db9a98eb2af7afd4bca7e2a75
Niamh Brockbank <niamh.brockbank@openbet.com>
2024-11-15 16:48:33 +0000

    Two-way bind input values

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,24 +1,38 @@
 import { useState } from "react";
 
-export default function Player({ name, symbol }) {
+export default function Player({ initialName, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
+  const [playerName, setPlayerName] = useState(initialName);
 
   function handleEditClick() {
-    setIsEditing(!isEditing);
+    setIsEditing((editing) => !editing);
   }
 
-  let playerName = <span className="player-name">{name}</span>;
+  function handleChangeName(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editablePlayerName = <span className="player-name">{playerName}</span>;
+  let btnCaption = "Edit";
 
   if (isEditing) {
-    playerName = <input type="text"></input>;
+    editablePlayerName = (
+      <input
+        type="text"
+        required
+        value={playerName}
+        onChange={handleChangeName}
+      ></input>
+    );
+    btnCaption = "Save";
   }
 
   return (
     <li>
       <span className="player">
-        {playerName}
+        {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
-        <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
+        <button onClick={handleEditClick}>{btnCaption}</button>
       </span>
     </li>
   );
